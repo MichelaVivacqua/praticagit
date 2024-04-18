1dc30f50608ce3f07c0845b0e1fe8e65ca1ba040
MichelaVivacqua <vivacquamichela@gmail.com>
2024-04-18 15:01:22 +0200

    vediamo

--- Conflitto.java
+++ Conflitto.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Conflitto {
+public class Conflitto {
+    public String vediamo;
+    public String che;
+    public String succedeora;
+
+    public  Conflitto (String vediamo, String che, String succedeora){
+        this.vediamo=vediamo;
+        this.che=che;
+        this.succedeora=succedeora;
+    }
 }
